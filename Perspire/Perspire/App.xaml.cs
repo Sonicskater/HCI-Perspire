78c0ca8954ad4e048f8314f3e0d149a0710026e3
Devon Hockley <devon@hockley.ca>
2020-03-05 16:57:47 -0700

    Never mind other crap is broken

--- Perspire/Perspire/App.xaml.cs
+++ Perspire/Perspire/App.xaml.cs
@@ -14,7 +14,7 @@ namespace Perspire
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
-            MainPage = new MainPage();
+            MainPage = new AppShell();
         }
 
         protected override void OnStart()
