b98605c9176a569c05076f2825c58f83e0701206
Devon Hockley <devon@hockley.ca>
2020-03-08 18:23:22 -0600

    Refactored a componenet and added programs screen

---
+++ Perspire/Perspire/Controls/NestedDropCard.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Perspire.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NestedDropCard : ContentView
+    {
+        public NestedDropCard()
+        {
+            InitializeComponent();
+        }
+        public View Top
+        {
+            get { return TopContent.Content; }
+            set { TopContent.Content = value; }
+        }
+
+        public View Bottom
+        {
+            get { return BottomContent.Content; }
+            set { BottomContent.Content = value; }
+        }
+
+
+
+    }
+}
