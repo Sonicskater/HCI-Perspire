785b0ef38f0d2fe91a5dc4eef84c0112edcfd9b4
Devon Hockley <devon@hockley.ca>
2020-03-08 23:01:19 -0600

    Made custom graph component.

---
+++ Perspire/Perspire/Controls/GraphedStat.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Perspire.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GraphedStat : ContentView
+    {
+        public string Previous
+        {
+            get
+            {
+                return previous.Text;
+            }
+            set
+            {
+                previous.Text = value;
+            }
+        }
+        public string Entry
+        {
+            get; set;
+        }
+        public GraphedStat()      
+        {
+  
+            InitializeComponent();
+        }
+
+        public string Units
+        {
+            get { return units1.Text; }
+            set { units1.Text = value;
+                units2.Text = value;
+            }
+        }
+
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+            System.Console.WriteLine(Entry);
+        }
+    }
+}
