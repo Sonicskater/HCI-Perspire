9ef8bee51d45192cb2aa92a7b95dbe3bc46d1ef7
Devon Hockley <devon@hockley.ca>
2020-04-03 14:24:34 -0600

    Remvoed last pieces of project template to clean up folder.

--- Perspire/Perspire/ViewModels/BaseViewModel.cs
+++ Perspire/Perspire/ViewModels/BaseViewModel.cs
@@ -6,13 +6,11 @@ using System.Runtime.CompilerServices;
 using Xamarin.Forms;
 
 using Perspire.Models;
-using Perspire.Services;
 
 namespace Perspire.ViewModels
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
-        public IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>();
 
         bool isBusy = false;
         public bool IsBusy
