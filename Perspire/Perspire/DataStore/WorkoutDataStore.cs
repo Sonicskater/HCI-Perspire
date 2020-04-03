00ed04554174538c807d0b7390720d56b99f79ec
Devon Hockley <devon@hockley.ca>
2020-04-03 14:18:16 -0600

    Refactored workoutlistviewmodel to rely on datastore singleton. Now we have the same data set across all workout related pages.

---
+++ Perspire/Perspire/DataStore/WorkoutDataStore.cs
@@ -0,0 +1,61 @@
+﻿using Perspire.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Perspire.DataStore
+{
+    class WorkoutDataStore : List<WorkoutGroupModel>
+    {
+        private WorkoutDataStore() : base()
+        {
+            var group = new WorkoutGroupModel("Back");
+
+            group.Add(new WorkoutModel
+            {
+                Name = "Workout 1",
+                Part = "Back",
+                ImageSrc = "tab_about.png",
+                Description = "back workout"
+            });
+
+            group.Add(new WorkoutModel
+            {
+                Name = "Workout 2",
+                Part = "Back",
+                ImageSrc = "tab_about.png",
+                Description = "back workout"
+            });
+            Add(group);
+
+            group = new WorkoutGroupModel("Shoulder");
+            group.Add(new WorkoutModel
+            {
+                Name = "Workout 3",
+                Part = "Shoulder",
+                ImageSrc = "tab_about.png",
+                Description = "back workout"
+            });
+
+            group.Add(new WorkoutModel
+            {
+                Name = "Workout 4",
+                Part = "Shoulder",
+                ImageSrc = "tab_about.png",
+                Description = "back workout"
+            });
+            Add(group);
+        }
+
+        // singleton data pattern; not great, but works for now
+        private static WorkoutDataStore me = null;
+        public static WorkoutDataStore Get()
+        {
+            if (me == null)
+            {
+                me = new WorkoutDataStore();
+            }
+            return me;
+        }
+    }
+}
--- Perspire/Perspire/ViewModels/Workouts/WorkoutListViewModel.cs
+++ Perspire/Perspire/ViewModels/Workouts/WorkoutListViewModel.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using Perspire.DataStore;
 using Perspire.Models;
 
 namespace Perspire.ViewModels
@@ -11,42 +12,11 @@ namespace Perspire.ViewModels
         public ObservableCollection<WorkoutGroupViewModel> WorkoutList { get; set; } = new ObservableCollection<WorkoutGroupViewModel>();
         public WorkoutListViewModel()
         {
-            var group = new WorkoutGroupModel("Back");
-            
-            group.Add(new WorkoutModel
+            var datastore = WorkoutDataStore.Get();
+            foreach (var i in datastore)
             {
-                Name = "Workout 1",
-                Part = "Back",
-                ImageSrc = "tab_about.png",
-                Description = "back workout"
-            });
-
-            group.Add(new WorkoutModel
-            {
-                Name = "Workout 2",
-                Part = "Back",
-                ImageSrc = "tab_about.png",
-                Description = "back workout"
-            });
-            WorkoutList.Add(new WorkoutGroupViewModel(group));
-
-            group = new WorkoutGroupModel("Shoulder");
-            group.Add(new WorkoutModel
-            {
-                Name = "Workout 3",
-                Part = "Shoulder",
-                ImageSrc = "tab_about.png",
-                Description = "back workout"
-            });
-
-            group.Add(new WorkoutModel
-            {
-                Name = "Workout 4",
-                Part = "Shoulder",
-                ImageSrc = "tab_about.png",
-                Description = "back workout"
-            });
-            WorkoutList.Add(new WorkoutGroupViewModel(group));
+                WorkoutList.Add(new WorkoutGroupViewModel(i));
+            }
         }
 
         public void ToggleVis(int index)
